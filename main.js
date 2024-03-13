a2a7caeab363acab5dd7f3720a0ee44c2786d565
ngovanD <ngovan09876@gmail.com>
2024-03-13 18:20:49 +0700

    uploat code

---
+++ main.js
@@ -0,0 +1,31 @@
+var fullName = 'Nay sinh nhật bay hả??';
+var age = 'Thiệt không?';
+var int = 'Hâm';
+
+
+//thông báo
+function showDialog() {
+    alert(fullName)
+}
+showDialog()
+
+alert(age);
+confirm('Thiệt hả!!!!!');
+
+
+setTimeout(function() { //hàm thời gian
+    prompt(int)
+}, 2000);
+
+console.log(age);
+//
+var myFriend = {
+    name: 'Chi'
+}
+console.log(myFriend)
+function Myclassmate(firstName, lastName, age) {
+    this.firstName;
+    this.lastName;
+    this.age;
+}
+
